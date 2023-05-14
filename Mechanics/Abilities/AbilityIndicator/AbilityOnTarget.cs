7155864fa4e2db0434d42d164a25b521dda49329
angeloje <angeloje97@outlook.com>
2023-05-13 22:38:34 -0700

    5/13/2023 Update

--- Mechanics/Abilities/AbilityIndicator/AbilityOnTarget.cs
+++ Mechanics/Abilities/AbilityIndicator/AbilityOnTarget.cs
@@ -10,19 +10,47 @@ namespace Architome.Indicator
         [Header("On Target Properties")]
         public Vector3 targetLocation;
 
+
+        bool firstIteration;
+        float groundPositionTimer;
+        float groundPosition;
+
         public override async void OnAbilityStartEnd(AbilityInfo ability, bool isActivated)
         {
             if (!isActivated) return;
             SetProjector(true);
+            firstIteration = false;
             await ability.HandleTargetLocation(StayOnTarget);
             SetProjector(false);
         }
 
         void StayOnTarget(Vector3 target)
         {
+            UpdateGroundPosition();
+            target.y = groundPosition;
 
-            var groundPosition = V3Helper.GroundPosition(target, groundLayer);
-            transform.position = groundPosition;
+
+            transform.position = target;
+
+            void UpdateGroundPosition()
+            {
+                if (!firstIteration)
+                {
+                    firstIteration = true;
+                    groundPosition = target.y;
+                }
+
+                if(groundPositionTimer > 0)
+                {
+                    groundPosition -= Time.deltaTime;
+                    return;
+                }
+
+                groundPositionTimer = 1f;
+
+                groundPosition = V3Helper.GroundPosition(target, groundLayer).y;
+            }
         }
+
     }
 }
