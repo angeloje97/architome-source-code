9f3708deea6ac1f07c1cc9027082dbd55b663df0
angeloje <angeloje97@outlook.com>
2023-08-20 23:53:56 -0700

    8/20/2023 Update

---
+++ EntityBehavior/Dialogue/DialogueCameraBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Architome.Assets.Source.Scripts.EntityBehavior.Dialogue
+{
+    public class DialogueCameraBehavior : MonoBehaviour
+    {
+        DialogueSource dialogueSource;
+        CameraAnchor cameraAnchor;
+        void Start()
+        {
+            GetDependencies();
+        }
+
+        void GetDependencies()
+        {
+            dialogueSource = GetComponentInParent<DialogueSource>();
+            cameraAnchor = CameraAnchor.active;
+        }
+    }
+}
