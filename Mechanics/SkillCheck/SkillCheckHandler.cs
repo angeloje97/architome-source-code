3f2bc02ab022054b9805e1d954fb5f53d80da7d5
angeloje <angeloje97@outlook.com>
2024-03-20 23:07:01 -0700

    3/20/2024 Update

---
+++ Mechanics/SkillCheck/SkillCheckHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Architome
+{
+    public class SkillCheckHandler : MonoActor
+    {
+        [SerializeField] bool chancePerSecond;
+        [SerializeField][Range(0f, 360f)] float angle;
+        [SerializeField][Range(0f, 360f)] float range;
+
+        public void HandleSkillChecks(TaskEventData eventData)
+        {
+
+        }
+    }
+}
