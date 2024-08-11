40106895180714edd4b2d798f0d1b269005c5934
angeloje <angeloje97@outlook.com>
2024-08-10 23:27:23 -0700

    8/10/2024 Update

--- DevTools/DevToolsManager.cs
+++ DevTools/DevToolsManager.cs
@@ -8,11 +8,25 @@ namespace Architome.DevTools
     {
         public static DevToolsManager active;
 
+        Actions actions;
+        Toggles toggles;
+
+        [Header("Components")]
+        public CanvasGroup actionsCG;
+        public CanvasGroup togglesCG;
+
         public void Awake()
         {
             SingletonManger.HandleSingleton(this.GetType(), gameObject, true, true, () => {
                 active = this;
             });
         }
+
+        private void OnValidate()
+        {
+            actions ??= GetComponentInChildren<Actions>();
+            toggles ??= GetComponentInChildren<Toggles>();
+        }
+
     }
 }
