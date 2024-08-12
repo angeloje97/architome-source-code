749c414a0eda83bc4d523c1252ea21b16953a756
angeloje <angeloje97@outlook.com>
2024-08-11 22:08:12 -0700

    8/11/2024 Update

---
+++ DevTools/DevTool.cs
@@ -0,0 +1,31 @@
+﻿using Architome.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Architome.DevTools
+{
+    public class DevTool : MonoBehaviour
+    {
+        [SerializeField] protected List<GameState> _availableStates;
+
+        public virtual List<GameState> availableStates
+        {
+            get
+            {
+                if(_availableStates == null)
+                {
+                    _availableStates = new();
+
+                    foreach(GameState state in Enum.GetValues(typeof(GameState)))
+                    {
+                        _availableStates.Add(state);
+                    }
+                }
+
+                return _availableStates;
+            }
+        }
+    }
+}
