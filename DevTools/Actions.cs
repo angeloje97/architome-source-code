00b42ff9a428b40dd64137b90ff128c8aacb566c
angeloje <angeloje97@outlook.com>
2024-08-12 22:36:05 -0700

    8/12/2024 Update

--- DevTools/Actions.cs
+++ DevTools/Actions.cs
@@ -1,10 +1,50 @@
+using Architome.Enums;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace Architome.DevTools
 {
-    public class Actions : MonoBehaviour
+    public class Actions : DevTool
     {
+        public class Request
+        {
+            public Dictionary<string, Type> validAttributes;
+
+            public Dictionary<string, object> currentParameters;
+            Action<Request> action;
+            public Request(Action action)
+            {
+                this.action += (request) => action();
+            }
+
+            public Request(Action<Request> action)
+            {
+                this.action += action;
+            }
+
+            public void Invoke(Dictionary<string, object> parameters)
+            {
+                this.currentParameters = parameters;
+                action?.Invoke(this);
+            }
+        }
+        public override List<GameState> availableStates
+        {
+            get
+            {
+                if (_availableStates == null)
+                {
+                    _availableStates = new()
+                    {
+                        GameState.Play
+                    };
+                }
+
+                return _availableStates;
+            }
+        }
     }
 }
--- DevTools/Toggles.cs
+++ DevTools/Toggles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Architome.DevTools
 {
-    public class Toggles : MonoBehaviour
+    public class Toggles : DevTool
     {
 
     }
