7497af3dd386026b4ca974602a5b7ff0f569ba27
angeloje <angeloje97@outlook.com>
2024-08-15 22:55:29 -0700

    8/15/2024 Update

--- DevTools/Toggles.cs
+++ DevTools/Toggles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,34 @@ namespace Architome.DevTools
 {
     public class Toggles : DevTool
     {
+        public class Request
+        {
+            bool currentState;
 
+            Action<Request> OnStateChange;
+
+            public Request(Action<bool> OnChangeState)
+            {
+                this.OnStateChange += (request) => {
+                    OnChangeState?.Invoke(currentState);
+                };
+            }
+
+            public Request(Action<Request> OnStateChange)
+            {
+                this.OnStateChange += (request) => OnStateChange?.Invoke(request);
+            }
+
+            public void SetState(bool state)
+            {
+                this.currentState = state;
+                OnStateChange?.Invoke(this);
+            }
+
+            public void ToggleState()
+            {
+                SetState(!currentState);
+            }
+        }
     }
 }
