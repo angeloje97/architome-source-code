447eb4c1a8b26b891df6b0f6a5aea9cd76de965a
angeloje <angeloje97@outlook.com>
2024-12-19 20:00:37 -0800

    12/19/2024 Update

--- UI/UI Elements/RangeSlider.cs
+++ UI/UI Elements/RangeSlider.cs
@@ -70,6 +70,8 @@ namespace Architome
                 currentValue.max = (int)currentValue.max;
             }
 
+            restrictions.ClampValues();
+
             currentValue.ClampValues(restrictions);
 
 
